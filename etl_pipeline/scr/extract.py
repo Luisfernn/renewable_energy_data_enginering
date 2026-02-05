44432a975cb9d47bdb2e2e4ddaec3a50034a1fe6
Luis Fernando <luisfernandowubi@gmail.com>
2026-02-05 10:11:40 -0300

    feat(extract): adiciona configs locais de logging que só aparecem ao executar a função localmente.

--- etl_pipeline/scr/extract.py
+++ etl_pipeline/scr/extract.py
@@ -50,3 +50,13 @@ def extract_data(file_path: Path = None):
     loggerinfo(f"{df.tail(10)}\n")
 
     return df
+
+
+if __name__ == "__main__":
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    extract_data()
