813ac681cb5625a844a21afc6763d7179c5f1d71
Luis Fernando <luisfernandowubi@gmail.com>
2026-02-09 08:13:47 -0300

    chore(transform): configura logger.setLevel(DEBUG) no bloco de execução local para visualizar detalhes de cada transformação

--- etl_pipeline/scr/transform/text.py
+++ etl_pipeline/scr/transform/text.py
@@ -135,7 +135,8 @@ if __name__ == "__main__":
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter("%(message)s"))
     logger.addHandler(handler)
-    logger.setLevel(logging.INFO) 
+    logger.setLevel(logging.DEBUG)
+    # logger.setLevel(logging.INFO) 
 
     df = normalize_columns_names()
     df = normalize_textual_columns(df)
