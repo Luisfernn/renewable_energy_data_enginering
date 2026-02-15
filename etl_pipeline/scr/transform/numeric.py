541b54780c009bcd04b28bca7aa36fb158b05452
Luis Fernando <luisfernandowubi@gmail.com>
2026-02-15 11:45:44 -0300

    feat(transform): cria função para arredondar para 2 casas após o ".'  os dados numéricos

---
+++ etl_pipeline/scr/transform/numeric.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from pathlib import Path
+
+import logging
+
+logger = logger.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.propagate = False
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+PROCESSED_DIR = BASE_DIR / 'data' / 'processed'
+INPUT_FILE = PROCESSED_DIR / 'renewable_energy_data_text.csv'
+OUTPUT_PATH = PROCESSED_DIR / 'renewable_energy_data_clean.csv'
+
+pd.set_option('display.max_columns', None)
+
+
+logger.debug(df.info())
+
+
+def round_metrics(df)
+
+metric_columns = [
+    'eletricity_generation_gwh',
+    'eletricity_installed_capacity_mw',
+    'heat_generation_tj',
+    'total_public_flows_usd_m',
+    'international_public_flows_usd_m',
+    'capacity_per_capta_w'
+]
+
+for col in metric_columns:
+    if col in df.columns:
+        df[col] = df[col].round(2)
+
+logger.debug(f"\nApós arrendondamento: \n{df[metric_columns].head()}")
+logger.info(f"✅ Dados numéricos arredondados!")     
+
+return df
