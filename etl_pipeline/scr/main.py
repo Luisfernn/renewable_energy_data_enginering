2ad0ee2dd8b0dffbe281c370139217d55c627238
Luis Fernando <luisfernandowubi@gmail.com>
2026-02-16 17:32:04 -0300

    fix(transform): corrige alguns erros de sintaxe em main.py
    - Remove "()" de parent na variável base_dir.
    
    - Substitui "." por "-" em log_dir_mkdir.
    
    - Normaliza o tipo de aspas na linha format na config de logging, antes usava 2 tipos de aspas diferentes, normalizei para apenas aspas simples.

--- etl_pipeline/scr/main.py
+++ etl_pipeline/scr/main.py
@@ -1,16 +1,16 @@
 import logging
 from pathlib import Path
 
-base_dir = Path(__file__).resolve().parent()
+base_dir = Path(__file__).resolve().parent
 output_dir = base_dir / 'data' / 'processed'
 
 log_dir = base_dir / 'data' / 'logs'
-log.dir.mkdir(parents=True, exist_ok=True)
+log_dir_mkdir(parents=True, exist_ok=True)
 
 
 logging.basicConfig(
     level = logging.INFO,
-    format='%(asctime)s | %(levelname)s | %(message)s",
+    format='%(asctime)s | %(levelname)s | %(message)s',
     handlers=[
         logging.FileHandler(LOG_DIR / "etl.log", mode="a", encoding="utf-8"),
         logging.StreamHandler()
